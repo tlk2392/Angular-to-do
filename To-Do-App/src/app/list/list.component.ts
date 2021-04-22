5978ff1ff60bd59f74bc02024181ce14ee380e42
Travis Krenzien <krenzien.travis@yahoo.com>
2021-04-21 18:17:56 -0600

    final

---
+++ To-Do-App/src/app/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  public tasklist: Task []=[];
+
+  ngOnInit(): void {
+  }
+
+  addTask(){
+    this.tasklist.push(new Task());
+
+  }
+
+  removeTask(index:number){
+    if(index > -1){
+      this.tasklist.splice(index,1);
+    }
+  }
+
+}
